a0d9179b67ffe6867a7b84b7823e7ed853f7dd09
ArchieTansaria <archietans.work@gmail.com>
2026-04-02 01:48:22 +0530

    chore: db config for worker

--- github-app/src/workers/index.ts
+++ github-app/src/workers/index.ts
@@ -1,3 +1,17 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+async function connectToDB(): Promise<void> {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log("[workers] DB successfully connected");
+  } catch (error) {
+    console.error("[workers] connection to db failed", error);
+  }
+}
+
+connectToDB();
+
 import './pr.worker.js';
 import './analysis.worker.js';
 
